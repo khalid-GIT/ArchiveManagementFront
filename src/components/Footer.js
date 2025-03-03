cf852c48d30a97875f366fc768301f59a54bfa69
khalid elkettani <khalid.elkettani@gmail.com>
2025-03-03 22:30:42 +0100

    Design first up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,15 @@ import React, {} from "react";
 function Footer() {
     return (
       <div >
-       - Footer
+                {/* /.content-wrapper */}
+            <footer className="main-footer">
+            <strong>Copyright © 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+            All rights reserved.
+            <div className="float-right d-none d-sm-inline-block">
+                <b>Version</b> 3.1.0-rc
+            </div>
+            </footer>
+
       </div>
     );
   }
