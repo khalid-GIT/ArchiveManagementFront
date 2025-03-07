1e239c3cdd74a250c91693c1bcf86e00094fedee
khalid elkettani <khalid.elkettani@gmail.com>
2025-03-07 23:10:00 +0100

    Fix link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
             <div>
             {/* /.content-wrapper */}
             <footer className="main-footer">
-                <strong>Copyright © 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+                <strong>Copyright © 2014-2020 <a href="https://adminlte.io">My Archive</a>.</strong>
                 All rights reserved.
                 <div className="float-right d-none d-sm-inline-block">
                 <b>Version</b> 3.1.0-rc
